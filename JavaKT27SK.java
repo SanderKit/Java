ad5e22c911264749deacd14208d3c372183864d5
SanderKit <75732340+SanderKit@users.noreply.github.com>
2021-06-07 13:53:45 +0300

    Add files via upload

---
+++ JavaKT27SK.java
@@ -0,0 +1,21 @@
+package sander_java;
+import java.util.Scanner;
+
+public class JavaKT27SK {
+	public static void main(String[] args) {
+			 int arve = 36;
+			 var protsent2 =  0.9;
+			 double protsent;
+			 double tipp;
+			 
+			 protsent = arve / protsent2;
+			 tipp = protsent - arve;
+			 System.out.println("Teie arve on: "+ arve);
+			 System.out.println("teie tipp on: "+ tipp);
+			 System.out.println("kogu summa :"+ protsent);
+			 }
+			
+		
+	}
+
+
